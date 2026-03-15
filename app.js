b30c926cb08a5030d8d6b8d7b5896140cf7d8ae2
nivull <biscaynemac@gmail.com>
2026-03-15 17:13:10 -0400

    fixed adding bug, trying to work out deleting

--- app.js
+++ app.js
@@ -1,11 +1,8 @@
-const item1 = document.getElementById("todo-1");
-const item2 = document.getElementById("todo-2");
 let trashCan = document.querySelectorAll(".trash");
 const form = document.getElementById("form");
 const todoInput = document.getElementById("todo-input");
 const todoList = document.getElementById("list");
 let items = [];
-items.push(item1);
 updateItems();
 trashCan.forEach((element)=> {
     addEventListener('click', (_, i)=>{
@@ -48,10 +45,9 @@ function createTodo(text) {
             </label>
 
         <label class="todo-text" for="todo-${todoNum}">${text}</label>
-            <button type="button" class="trash">
+            <button type="button" class="trash" data-index="${todoNum}">
                 <svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#e3e3e3">
                 <path d="M280-120q-33 0-56.5-23.5T200-200v-520h-40v-80h200v-40h240v40h200v80h-40v520q0 33-23.5 56.5T680-120H280Zm400-600H280v520h400v-520ZM360-280h80v-360h-80v360Zm160 0h80v-360h-80v360ZM280-720v520-520Z"/></svg></button>`;   
-    todoList.append(li);
     items.push(li);      
     trashCan = document.querySelectorAll(".trash");
     updateItems();
@@ -60,16 +56,21 @@ function createTodo(text) {
 function updateItems() {
 
     for(let x of items) {
-        todoList.append(x.innerHTML);
+        todoList.append(x);
     }
 
 
     //trash can code
     if(trashCan.length > 0) {
-        trashCan[trashCan.length - 1].addEventListener('click', (_,i)=>{
-            console.log(items.length + " before");
+        console.log("trash")
+        trashCan[trashCan.length - 1].addEventListener('click', (e)=>{
+            const i = [...trashCan].indexOf(e);
+            console.log([...trashCan]);
+            console.log(e);
+            console.log(i);
+            //console.log(items.length + " before");
             items.splice(i, 1);
-            console.log(items.length + " after");
+            //console.log(items.length + " after");
         });
     }
 
